47dc14b9d24059f3ddefc598d876934dd292d7e2
Youngsuk Kim <1990youngsuk@gmail.com>
2021-04-10 14:13:13 +0900

    210410 2:10pm not done anything yet except writing some notes

--- mysite/survey/settings.py
+++ mysite/survey/settings.py
@@ -49,10 +49,10 @@ DATABASES = {
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
@@ -64,4 +64,3 @@ STATICFILES_DIRS = []
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/surveys/"
 INTERNAL_IPS = ["127.0.0.1"]
-
